e37ddb783e2fcde45632d4bd42fe2e250f7ddaed
mcheng89 <mcheng89@gmail.com>
2026-04-10 23:15:22 -0500

    updated hints for models

--- models/attention_lstm.py
+++ models/attention_lstm.py
@@ -1,13 +1,19 @@
-# extra info
+"""
+To work with the jumbo i did prob should have something that follows this
 
-def forward(self, features, captions):  
-        """
-        REQUIRED SHAPES:
-        Input:
-            features: [batch_size, embed_size] (from Encoder)
-            captions: [batch_size, seq_length - 1] (target tokens, shifted by train.py)
-        Output:
-            predictions: [batch_size, seq_length - 1, vocab_size]
-        """
-        # logic here
-        pass
+1 forward(self, features, captions)
+   Used by train.py for training.
+   Inputs:
+       features: [batch_size, embed_size] (from Encoder)
+       captions: [batch_size, seq_length - 1] (Target tokens, shifted by train.py)
+   Outputs:
+       predictions: [batch_size, seq_length - 1, vocab_size]
+
+2 sample(self, features, max_length=20)
+   Used by evaluate.py to generate text for a brand new image.
+   Inputs:
+       features: [1, embed_size] (Single image feature vector)
+       max_length: int (Maximum words to generate)
+   Outputs:
+       predicted_ids: [1, generated_length] (tensor of the integer word IDs)
+"""
